e4fc4ea969f7ec874ccb1f54d78582bab2ef4b36
MurakamiHonami <mnv2525@gmail.com>
2025-10-26 16:26:29 +0900

    コードを修正

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Example from './App.jsx'; // App.jsxをインポート
+import Example from './App.jsx';
 
-// DOM要素（index.html内の <div id="root">）を取得
 const container = document.getElementById('root');
 if (container) {
-    // React 18以降の新しいAPIを使用してルートを作成し、コンポーネントをレンダリング
     const root = createRoot(container);
     root.render(
         <React.StrictMode>
